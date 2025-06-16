f06c9355bc17d9d1fffc998b02a6f7f197ed1374
Atish Jain <atish_jain@yahoo.com>
2025-06-16 11:17:33 +0530

    Add files via upload

---
+++ 001_helloworld.c
@@ -0,0 +1,15 @@
+/*
+Program name: 001_helloworld.c
+Task: Displays Welcome Message
+Author: Atish Jan
+Date: 14-Jun-25
+*/
+
+# include <stdio.h>
+int main() //starting point of program execution
+{
+    printf("Welcome to C Programming...\n");
+    printf("This is My First C Program");
+
+ return 0;
+}
---
+++ 002_vardemo.c
@@ -0,0 +1,17 @@
+/*
+Program name: 002_vardemo.c
+Task: declares int variable and displays
+Author: Atish Jan
+Date: 16-Jun-25
+*/
+
+# include <stdio.h>
+int main() //starting point of program execution
+{
+    int n=10; //variable declaration
+
+    printf("%d",n);
+    printf("\nThe value of n is : %d",n);
+
+ return 0;
+}
